96d07e246229fc0417c81ea34b087ea4db78fed7
Gleydson Gibson <ggleydson@icloud.com>
2026-08-15 09:41:02 +0000

    mudanças de frases dentro do arquivo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1> Deus é maravilhoso! </h1>
+    return (
+        <>
+            <h1> Deus é maravilhoso de todas as formas! </h1>
+            <h1> Que não nos falte fé, esperança e amor! </h1>
+        </>
+    )
 }
 
 export default Home;
